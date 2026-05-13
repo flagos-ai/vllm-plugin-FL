3719e5d23a3ea7e43e999c1aa994f29ff76c296e
jiapengfei <18253160554@163.com>
2026-05-13 11:07:25 +0800

    fix code bugs

--- vllm_fl/utils.py
+++ vllm_fl/utils.py
@@ -210,7 +210,7 @@ _load_op_config_from_env()
 
 class DeviceInfo:
     def __init__(self):
-        self.device = DeviceDetector()     
+        self.device = DeviceDetector()
         self.supported_device = ["nvidia", "ascend", "metax", "mthreads", "kunlunxin", "hygon"]
         backend.set_torch_backend_device_fn(self.device.vendor_name)
 
