90148b18f0413e9880620723ba9bfe052bd8d0cf
ruanchunyang <ruanchunyang@tsingmicro.com>
2026-06-17 17:37:40 +0800

    fix init file error.

--- vllm_fl/patches/__init__.py
+++
@@ -1,6 +0,0 @@
-from .glm_moe_dsa import apply_platform_patches as glm5_platform
-from .rank_logging import apply_rank_logging_patch
-
-def apply_all_patches():
-    glm5_platform()
-    apply_rank_logging_patch()
