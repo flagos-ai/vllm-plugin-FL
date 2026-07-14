f1b93ccee10affb01aeb327cc81a17986da48a66
cyber-pioneer <chenzhuo@tju.edu.cn>
2026-07-14 18:48:52 +0800

    fix code

--- vllm_fl/dispatch/backends/flaggems/impl/attention.py
+++ vllm_fl/dispatch/backends/flaggems/impl/attention.py
@@ -74,7 +74,7 @@ class AttentionFLBackend(AttentionBackend):
 
     @staticmethod
     def get_name() -> str:
-        return "CUSTOM"
+        return "FL"
 
     @classmethod
     def supports_attn_type(cls, attn_type: str) -> bool:
