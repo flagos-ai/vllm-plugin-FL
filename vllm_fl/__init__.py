511a39a48626a39ee868957abc48ee16f91031e8
luoyoucai <136922441@qq.com>
2026-07-28 10:58:30 +0800

    Remove unnecessary modified code

--- vllm_fl/__init__.py
+++ vllm_fl/__init__.py
@@ -146,11 +146,10 @@ def register_router():
     from vllm_fl.ops.fused_moe.router import replace_router_with_fl
     replace_router_with_fl()
 
-
-
 def register_model():
     """Register FL-specific models not yet upstream."""
     from vllm import ModelRegistry
+
     _register_flagcx_connector()
 
     # Register OOT quant kernels so kernel selection can find them
